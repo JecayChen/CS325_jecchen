6a571685f8c5e131faba377fbbe7475c7c403b25
Jecay Chen <triplx@TriplX.lan>
2024-01-28 00:14:13 -0600

    edit lines 4-17; added replay function

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,17 @@
 import random
 
+again = bool(True)
+correct = bool(True)
 print("Coinflip")
 
-rand = int(random.randint(0,1))
-if rand == 0:{
-    print("Heads")
-}
-else:{
-    print("Tails")
-}
+while again:
+    rand = int(random.randint(0,1))
+    if rand == 0:{
+        print("Heads")
+    }
+    else:{
+        print("Tails")
+    }
+    while not correct: 
+        again = input("Flip again (True/False)?:")
+    correct = True
